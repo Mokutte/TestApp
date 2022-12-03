13649034654f7939b4f8cdd2866f8341435690f0
Yahikolo <d.tsybin@picasso-software.ru>
2022-12-03 19:42:18 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import DepartmentCart from "./departmentCart";
+import { ReactComponent as PlusSvg } from "./image/plusSvg.svg";
+import { useEffect, useState } from "react";
+import { MainContext } from "./context";
+import moment from "moment";
 
 function App() {
+  const [departments, setDepartments] = useState([
+    {
+      id: 1,
+      changes: false,
+      departmentName: "Отдел продаж",
+      directorName: "Макаренко Иван Сергеевич",
+      employeesNum: 56,
+    },
+    {
+      id: 2,
+      changes: false,
+      departmentName: "IT отдел",
+      directorName: "Макаренко Иван Сергеевич",
+      employeesNum: 72,
+    },
+    {
+      id: 3,
+      changes: false,
+      departmentName: "Отдел маркетинга",
+      directorName: "Макаренко Иван Сергеевич",
+      employeesNum: 56,
+    },
+  ]);
+  const [error, setError] = useState();
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MainContext.Provider value={{ setError, setDepartments, departments }}>
+      <div className="App">
+        <div className="container">
+          {departments.map((el) => (
+            <DepartmentCart
+              key={el.departmentName}
+              changes={el.changes}
+              id={el.id}
+              departmentName={el.departmentName}
+              directorName={el.directorName}
+              employeesNum={el.employeesNum}
+            />
+          ))}
+          <button
+            className="addBlock"
+            onClick={() =>
+              setDepartments([
+                ...departments,
+                {
+                  id: moment().format('LTS'),
+                  changes: true,
+                  departmentName: "new departament",
+                  directorName: "",
+                  employeesNum: 0,
+                },
+              ])
+            }
+          >
+            <PlusSvg />
+          </button>
+        </div>
+      </div>
+    </MainContext.Provider>
   );
 }
 
---
+++ src/departmentCart.jsx
@@ -0,0 +1,99 @@
+import "./App.css";
+import { ReactComponent as EditIcon } from "./image/editImage.svg";
+import { ReactComponent as CheckIcon } from "./image/check.svg";
+import { useState, useContext } from "react";
+import { MainContext } from "./context";
+
+export default function DepartmentCart(props) {
+  const [newDepartmentName, setNewDepartmentName] = useState();
+  const [newDirectorName, setNewDirectorName] = useState();
+  const [newEmployeesNum, setNewEmployeesNum] = useState();
+  const { setDepartments, departments } = useContext(MainContext);
+
+  const switchChanges = () => {
+    const newDepartments = departments.map((el) =>
+       el.id === props.id 
+        ? { ...el, changes: !el.changes }
+        : el
+    );
+    setDepartments(newDepartments);
+  };
+
+  const updateCart = () => {
+      const newDepartments = departments.map((el) =>
+        el.id === props.id 
+          ? { id: el.id,
+              changes: false,
+              departmentName: newDepartmentName ? newDepartmentName : el.departmentName,
+              directorName: newDirectorName ? newDirectorName : el.directorName,
+              employeesNum: newEmployeesNum ? newEmployeesNum : el.employeesNum,
+            }
+          : el
+      );
+      console.log(newDepartments)
+      setDepartments(newDepartments);
+  };
+
+
+
+  return (
+    <div className="departmentCart">
+      <div className="header">
+        {props.changes ? (
+          <>
+            <input
+              type="text"
+              placeholder="Название подраздела"
+              className="departmentNameInput"
+              value={newDepartmentName}
+              onChange={(e) => setNewDepartmentName(e.target.value)}
+            />
+            <button className="editBtn" onClick={updateCart}>
+              <CheckIcon />
+            </button>
+          </>
+        ) : (
+          <>
+            <p className="departmentName">{props.departmentName}</p>
+            <button className="editBtn" onClick={switchChanges}>
+              <EditIcon />
+            </button>
+          </>
+        )}
+      </div>
+      {props.changes ? (
+        <div className="directorBlock">
+          <span className="title">Руководитель</span>
+          <input
+            type="text"
+            placeholder="Имя руководителя"
+            className="directorNameInput"
+            value={newDirectorName}
+            onChange={(e) => setNewDirectorName(e.target.value)}
+          />
+        </div>
+      ) : (
+        <div className="directorBlock">
+          <span className="title">Руководитель</span>
+          <p className="directorName">{props.directorName}</p>
+        </div>
+      )}
+      {props.changes ? (
+        <div className="employeesBlock">
+          <span className="title">Количечтво сотрудников</span>
+          <input
+            type="number"
+            className="employeesNumInput"
+            value={newEmployeesNum}
+            onChange={(e) => setNewEmployeesNum(e.target.value)}
+          />
+        </div>
+      ) : (
+        <div className="employeesBlock">
+          <span className="title">Количечтво сотрудников</span>
+          <p className="employeesNum">{props.employeesNum}</p>
+        </div>
+      )}
+    </div>
+  );
+}
